c49d0b76570855662ef0ffb18ab47ff63aa31961
ashwin <abc.@gmail.com>
2024-04-15 11:41:44 +0530

    latest

--- models/mart/Project_status.sql
+++ models/mart/Project_status.sql
@@ -21,7 +21,7 @@ SELECT
         WHEN project_status BETWEEN 70 AND 80 THEN 'In Progress'
         WHEN project_status BETWEEN 80 AND 99 THEN 'Near Completion'
         WHEN project_status = 100 THEN 'Completion'
-        ELSE 'Unknown' -- Handle other cases if any
+        ELSE 'Unknown' 
     END AS status_category,
     ABS(DATEDIFF(DAY, GETDATE(), to_date)) AS days_until_completion
 FROM
